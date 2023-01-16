0d0a939acef760e11b66fc6bf36854390cc7d686
Costicus <yaremenko.konstantin@gmail.com>
2023-01-16 16:28:33 +0000

    To do list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
+import React, {useState} from 'react'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [items, setItems] = useState([
+    {id: 1, name: "Buy shopping"},
+    {id: 2, name: "Clean bathroom"},
+    {id: 3, name: "Car's MOT"}
+  ])
+
+const [newItem, setNewItem] = useState("")
+
+const handleItemInput = (evt) => {
+  setNewItem(evt.target.value)
+}
+
+const saveNewItem = (evt) => {
+  evt.preventDefault()
+  const newItemObj = {id: Date.now(), name: newItem}
+  const newListOfItems = [...items, newItemObj]
+  setItems(newListOfItems)
+  setNewItem("")
+}
+
+const addItem = (id) => {
+  const newItems = items.filter((item) => item.id !== id)
+  setItems(newItems) 
+}
+
+  const todoListItems = items.map((item) => {
+    return (
+    <li key={item.id}>
+      {item.name}
+    <button onClick={() => {addItem(item.id)}}>Completed</button>
+    </li>
+    )
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>TODO List - {items.length === 0 ? "Finished" : "Work in process!"}</h1>
+      <hr></hr>
+
+      <form onSubmit={saveNewItem}>
+        <label htmlFor="new-tem">Add a new to do</label>
+        <input id="new-item type="type="text" value={newItem} onChange={handleItemInput}></input>
+        <input type="submit" value="Save New Item"></input>
+      </form>
+
+      <ul>
+        {todoListItems}
+      </ul>
+
     </div>
   );
 }
+
 
 export default App;
